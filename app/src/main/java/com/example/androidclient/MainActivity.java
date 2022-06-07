6c93800fde51c19ed3428ee13ff4006558404aed
KehatSudri <keatsudri@gmail.com>
2022-06-07 12:25:47 +0300

    sned and recieve msg xml rotate menue animation type msg xml

--- app/src/main/java/com/example/androidclient/MainActivity.java
+++ app/src/main/java/com/example/androidclient/MainActivity.java
@@ -1,12 +1,18 @@
 package com.example.androidclient;
 
 import android.content.Intent;
-import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.Button;
 import android.widget.EditText;
 
+import androidx.appcompat.app.AppCompatActivity;
+
 public class MainActivity extends AppCompatActivity {
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,7 +29,7 @@ public class MainActivity extends AppCompatActivity {
 
         Button loginBtn = findViewById(R.id.login_Button);
         loginBtn.setOnClickListener(p -> {
-            if(Validate() == 0) {
+            if (Validate() == 0) {
                 Intent I = new Intent(this, ContactList.class);
                 startActivity(I);
             }
@@ -31,16 +37,15 @@ public class MainActivity extends AppCompatActivity {
     }
 
 
-
     public int Validate() {
         int flag = 0;
         EditText tvContact_username = findViewById(R.id.login_editTextUserName);
-        if(tvContact_username.getText().toString().length() == 0) {
+        if (tvContact_username.getText().toString().length() == 0) {
             tvContact_username.setError("Should not be empty");
             flag = 1;
         }
         EditText tv_password1 = findViewById(R.id.login_editTextPassword);
-        if(tv_password1.getText().toString().length() == 0) {
+        if (tv_password1.getText().toString().length() == 0) {
             tv_password1.setError("Should not be empty");
             flag = 1;
         }
