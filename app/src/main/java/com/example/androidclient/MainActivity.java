8d782e815cbbe02931d1a59ca2bc4625bd6ff979
KehatSudri <keatsudri@gmail.com>
2022-06-16 22:59:19 +0300

    FireBase

--- app/src/main/java/com/example/androidclient/MainActivity.java
+++ app/src/main/java/com/example/androidclient/MainActivity.java
@@ -6,64 +6,46 @@ import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import com.example.androidclient.api.UserAPI;
-import com.example.androidclient.entities.User;
+import com.google.firebase.iid.FirebaseInstanceId;
 
 public class MainActivity extends AppCompatActivity {
-    UserAPI userAPI;
-    EditText tvContact_username;
-    EditText tv_password;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        userAPI = new UserAPI();
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        tvContact_username = findViewById(R.id.login_editTextUserName);
-        tv_password = findViewById(R.id.login_editTextPassword);
 
         Button btnMoveToRegister = findViewById(R.id.buttonMoveToRegister);
         btnMoveToRegister.setOnClickListener(p -> {
             Intent i = new Intent(this, Register.class);
             startActivity(i);
+            finish();
         });
 
         Button loginBtn = findViewById(R.id.login_Button);
         loginBtn.setOnClickListener(p -> {
             if (Validate() == 0) {
-                Intent intent = new Intent(this, ContactList.class);
-                User user = new User(tvContact_username.getText().toString(), tv_password.getText().toString());
-                userAPI.login(user);
-                while(user.getToken() == null) {
-                }
-                if(user.getToken() == "error") {
-                    Toast toast = Toast.makeText(MyApplication.context, "Wrong user name or password", Toast.LENGTH_LONG);
-                    toast.show();
-                }
-                else if(user.getToken() == "error2") {
-                    Toast toast = Toast.makeText(MyApplication.context, "Can't reach server", Toast.LENGTH_LONG);
-                    toast.show();
-                }
-                else {
-                    intent.putExtra("userName", user.getUsername());
-                    intent.putExtra("token", user.getToken());
-                    startActivity(intent);
-                }
+                Intent I = new Intent(this, ContactList.class);
+                startActivity(I);
             }
+        });
+        FirebaseInstanceId.getInstance().getInstanceId().addOnSuccessListener(MainActivity.this,instanceIdResult -> {
+            String newToken = instanceIdResult.getToken();
         });
     }
     public int Validate() {
         int flag = 0;
+        EditText tvContact_username = findViewById(R.id.login_editTextUserName);
         if (tvContact_username.getText().toString().length() == 0) {
             tvContact_username.setError("Should not be empty");
             flag = 1;
         }
-        if (tv_password.getText().toString().length() == 0) {
-            tv_password.setError("Should not be empty");
+        EditText tv_password1 = findViewById(R.id.login_editTextPassword);
+        if (tv_password1.getText().toString().length() == 0) {
+            tv_password1.setError("Should not be empty");
             flag = 1;
         }
         return flag;
